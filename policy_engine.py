712f85e76a82288e51b91cc1c7c678ce6faf192b
Michael Wang <mh5wang@edu.uwaterloo.ca>
2021-06-25 23:55:12 -0400

    Implemented a unit test suite using unittest and mock

--- policy_engine.py
+++ policy_engine.py
@@ -3,16 +3,16 @@ import sys
 import logging
 import json
 
+from azure.mgmt.resource.policy import PolicyClient
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.resource.policy.models import PolicyAssignment
+from azure.identity import ClientSecretCredential
+
 # import dotenv to gain the ability to get environment variables from a .env file
 from dotenv import load_dotenv
 load_dotenv()
 
-from azure.mgmt.resource.policy import PolicyClient
-from azure.mgmt.resource import ResourceManagementClient
 
-from azure.mgmt.resource.policy.models import PolicyAssignment
-
-from azure.identity import ClientSecretCredential
 
 
 logging.basicConfig(stream=sys.stdout,
@@ -22,6 +22,7 @@ logging.basicConfig(stream=sys.stdout,
 LOCATION = 'eastus'
 GROUP_NAME = 'sample'
 
+# get the environment variables
 client_id = os.environ.get("AZURE_CLIENT_ID")
 client_secret = os.environ.get("AZURE_CLIENT_SECRET")
 tenant_id = os.environ.get("AZURE_TENANT_ID")
@@ -29,62 +30,67 @@ subscription_id = os.environ.get("AZURE_SUBSCRIPTION_ID")
 
 
 class PolicyEngine:
-    def __init__(self):
-        self.credentials = ClientSecretCredential(tenant_id=tenant_id, client_id=client_id, client_secret=client_secret)
+    def __init__(self, credentials):
+        self.credentials = credentials
 
     def assign_policy(self):
         policy_client = PolicyClient(self.credentials, subscription_id)
 
         # Create details for the assignment
-        policyAssignmentDetails = PolicyAssignment(display_name="Audit VMs without managed disks Assignment",
+        policy_assignment_details = PolicyAssignment(display_name="Audit VMs without managed disks Assignment",
                                                    policy_definition_id="/providers/Microsoft.Authorization/policyDefinitions/06a78e20-9358-41c9-923c-fb736d382a4d",
                                                    scope="/subscriptions/" + subscription_id,
                                                    description="Shows all virtual machines not using managed disks")
 
         # Create new policy assignment
-        policyAssignment = policy_client.policy_assignments.create("/subscriptions/" + subscription_id,
-                                                                   "audit-vm-manageddisks", policyAssignmentDetails)
+        response = policy_client.policy_assignments.create("/subscriptions/" + subscription_id,
+                                                                   "audit-vm-manageddisks", policy_assignment_details)
 
-        # Show results
-        print(policyAssignment)
+        return response
 
     def delete_assignment(self):
         policy_client = PolicyClient(self.credentials, subscription_id)
 
-        policy_client.policy_assignments.delete("/subscriptions/" + subscription_id, "audit-vm-manageddisks")
+        response = policy_client.policy_assignments.delete("/subscriptions/" + subscription_id, "audit-vm-manageddisks")
+        return response
 
     def create_resource_group(self):
         resource_client = ResourceManagementClient(self.credentials, subscription_id)
-        resource_client.resource_groups.create_or_update(GROUP_NAME, {'location': LOCATION})
+        response = resource_client.resource_groups.create_or_update(GROUP_NAME, {'location': LOCATION})
+        return response
 
     def create_policy_definition(self):
         policy_client = PolicyClient(self.credentials, subscription_id)
         with open("definitions/AuditStorageAccounts.json") as f:
-            policy_client.policy_definitions.create_or_update("hello", json.load(f))
+            response = policy_client.policy_definitions.create_or_update("hello", json.load(f))
+        return response
 
     def delete_policy_definition(self):
         policy_client = PolicyClient(self.credentials, subscription_id)
-        policy_client.policy_definitions.delete("hello")
+        response = policy_client.policy_definitions.delete("hello")
+        return response
 
 
-def main(func=5):
+def main(func=1):
     try:
-        engine = PolicyEngine()
+        credentials = ClientSecretCredential(tenant_id=tenant_id, client_id=client_id, client_secret=client_secret)
+
+        engine = PolicyEngine(credentials)
         # assign policy
         if func == 1:
-            engine.assign_policy()
+            print(engine.assign_policy())
         # delete policy assignment
         elif func == 2:
-            engine.delete_assignment()
+            print(engine.delete_assignment())
         # create resource group
         elif func == 3:
-            engine.create_resource_group()
+            print(engine.create_resource_group())
         # create policy definition based on template
         elif func == 4:
-            engine.create_policy_definition()
+            print(engine.create_policy_definition())
         # delete policy definition by name
         elif func == 5:
-            engine.delete_policy_definition()
+            print(engine.delete_policy_definition())
         else:
             pass
 
@@ -94,10 +100,4 @@ def main(func=5):
 
 
 if __name__ == "__main__":
-    # import argparse
-    #
-    # parser = argparse.ArgumentParser(description="Specify Policy Engine Function")
-    # # parser.add_argument("--function", required=True, help="which function to use")
-    # args = parser.parse_args()
-
     main()
---
+++ unit_tests.py
@@ -0,0 +1,39 @@
+import policy_engine
+import unittest
+from unittest import mock
+
+import os
+from azure.identity import ClientSecretCredential
+
+# import dotenv to gain the ability to get environment variables from a .env file
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class PolicyTestCase(unittest.TestCase):
+    def setUp(self):
+        client_id = os.environ.get("AZURE_CLIENT_ID")
+        client_secret = os.environ.get("AZURE_CLIENT_SECRET")
+        tenant_id = os.environ.get("AZURE_TENANT_ID")
+
+        credentials = ClientSecretCredential(tenant_id=tenant_id, client_id=client_id, client_secret=client_secret)
+
+        self.inst = policy_engine.PolicyEngine(credentials)
+
+    def test_assign_policy_passes(self):
+        self.assertTrue("Shows all virtual machines not using managed disks" in str(self.inst.assign_policy()))
+
+    def test_invalid_credentials(self):
+        self.inst.assign_policy = mock.Mock(side_effect=Exception('This is broken'))
+
+        with self.assertRaises(Exception) as context:
+            self.inst.assign_policy()
+
+        self.assertTrue('This is broken' in str(context.exception))
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
